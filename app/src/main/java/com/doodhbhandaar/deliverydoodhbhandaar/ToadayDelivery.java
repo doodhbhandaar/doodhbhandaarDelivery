ed48f70115e7c6a92802ea09d37a13288faf3480
Ankush <thakurankush947@gmail.com>
2019-02-02 22:57:25 +0530

    first commit1

---
+++ app/src/main/java/com/doodhbhandaar/deliverydoodhbhandaar/ToadayDelivery.java
@@ -0,0 +1,13 @@
+package com.doodhbhandaar.deliverydoodhbhandaar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToadayDelivery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toaday_delivery);
+    }
+}
